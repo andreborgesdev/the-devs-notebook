5155fb5d4c301a989c0335587b3be2da64b79b53
André Borges <a-filipe@hotmail.fr>
2025-06-05 12:24:18 +0200

    refactor: clean up comments and improve code readability in ContentPage component

--- src/app/[...slug]/page.tsx
+++ src/app/[...slug]/page.tsx
@@ -61,25 +61,21 @@ export default async function ContentPage({
   const getNextHeadingId = createHeadingIdGetter();
 
   const prettyCodeOptions = {
-    // Use one of Shiki's packaged themes
     theme: {
       dark: "dark-plus",
       light: "github-light",
     },
-    // Callback hooks for customization if needed
+    defaultLang: "plaintext",
+    keepBackground: false,
     onVisitLine(node: any) {
-      // Prevent lines from collapsing in `display: grid` mode, and allow empty
-      // lines to be copy/pasted
       if (node.children.length === 0) {
         node.children = [{ type: "text", value: " " }];
       }
     },
     onVisitHighlightedLine(node: any) {
-      // Each line node by default has `class="line"`.
       node.properties.className.push("highlighted");
     },
     onVisitHighlightedWord(node: any) {
-      // Each word node has no className by default.
       node.properties.className = ["word"];
     },
     transformers: [
